c1a462ee566327194c10717a2e0362ce7d71a152
argprog <alvarezlaura1984@gmail.com>
2023-02-04 10:52:11 -0300

    agrego ResponseBody en AuthController

--- src/main/java/com/backend/portfolio/Security/Controller/AuthController.java
+++ src/main/java/com/backend/portfolio/Security/Controller/AuthController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -74,6 +75,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @ResponseBody
     public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
